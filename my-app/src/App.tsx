07eaa451360d7e98c33fbeabfa368b0e9537ee6a
AYOMIFROSH <rhymeanabel@gmail.com>
2025-10-03 00:22:41 +0100

    landipage kronosys updated

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import KronosisLanding from "./pages/KronosisLanding";
 
-
 export default function App() {
   return (
     <BrowserRouter>
